0ca5480199db6d71cc3cc8f67adf1e4f4a966fbf
luis <luisnavarrodelangel@gmail.com>
2025-09-05 15:25:28 -0400

    adding workshops

---
+++ vue.js
@@ -0,0 +1,41 @@
+const { createApp } = Vue;
+const { createVuetify } = Vuetify;
+
+const App = {
+  data() {
+    return {
+      drawer: true,
+      tab: 0,
+      workshops: [
+        { title: "Workshop 1: Soundwalk, McMaster in 2011", icon: "mdi-walk" },
+        { title: "Workshop 2: Archival Sounds", icon: "mdi-music-note" },
+        { title: "Workshop 3: Creating Soundwalks", icon: "mdi-map-marker" }
+      ]
+    };
+  }
+}
+  
+
+// Vuetify theme setup
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'customTheme',
+    themes: {
+      customTheme: {
+        dark: false,
+        colors: {
+          primary: '#D00000',
+          secondary: '#ff9800',
+          accent: '#9c27b0',
+          background: '#f5f5f5',
+          error: '#f44336',
+        },
+      },
+    },
+  },
+  typography: {
+    defaultFontFamily: 'Lato, Arial, sans-serif',
+  },
+});
+
+createApp(App).use(vuetify).mount("#app");
